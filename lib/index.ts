64679aed5a2c642b55795b353cf5f5e695b5066a
Kaelan Smith <kaelancsmith@gmail.com>
2023-10-12 19:11:35 -0700

    update index file after all package adjustments

--- lib/index.ts
+++ lib/index.ts
@@ -9,6 +9,7 @@ export { default as BlockConfigProvider } from "./context/blockConfig";
 export { GlobalsProvider, useGlobals } from "./context/GlobalsContext";
 
 // Components
+export { BlocksPage } from "./BlocksPage";
 export { default as Blocks } from "./Blocks";
 export { default as Block } from "./Block";
 export { default as Container } from "./components/Container";
@@ -18,21 +19,16 @@ export { default as ErrorPage } from "./components/ErrorPage";
 export { AdminBar } from "./components/AdminBar";
 
 // Hooks
-export { useGlobalConfig } from "./hooks/useGlobalConfig";
 export { useBlockConfig } from "./hooks/useBlockConfig";
 export { useBlockStyleBuilder } from "./hooks/useBlockStyleBuilder";
-export { useFetchGraphAPI } from "./hooks/useFetchGraphAPI";
-export { useFetchRestAPI } from "./hooks/useFetchRestAPI";
 export { useUser } from "./hooks/useUser";
 
-// Getters
-export { getPage } from "./getters/getPage";
-export { getPreviewData } from "./getters/getPreviewData";
-export { getPost } from "./getters/getPost";
-export { getPosts } from "./getters/getPosts";
-export { getPaths } from "./getters/getPaths";
-export { getMenus } from "./getters/getMenus";
-export { getACFOptions } from "./getters/getACFOptions";
+// Preview mode
+export { getPreviewData } from "./preview/getPreviewData";
+export {
+  default as BlockPreviewPage,
+  getServerSideProps as BlockPreviewPageSSP,
+} from "./preview/BlockPreviewPage";
 
 // Utils
 export { deepMerge } from "./utils/deepMerge";
@@ -40,6 +36,3 @@ export { classNames } from "./utils/classNames";
 
 // API
 export { default as apiRouter } from "./api/apiRouter";
-
-// Preview Block for iFrames
-export * from "./preview-block";
