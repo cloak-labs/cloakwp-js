c302f2de4885c5079d66f66963dea01ea1cd0fa5
Kaelan Smith <kaelancsmith@gmail.com>
2024-01-27 23:04:44 -0800

    change workspace:* dep versions to hard numbers

--- dist/WPBlockRenderer.js
+++ dist/WPBlockRenderer.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.WPBlockRenderer = void 0;
 const cloakcms_1 = require("cloakcms");
 /**
- * A tiny wrapper around the BlockRenderer class from `render-blocks`, simply
+ * A tiny wrapper around the BlockRenderer class from `@kaelan/render-blocks`, simply
  * for the purposes of setting the `TBlockData` type parameter to default to CloakWP's
  * `RestApiBlockData` type, saving users from having to manually specify this type param.
  * Having users use this wrapper also sets us up for future WP-specific customizations
--- dist/wpBlockStyleBuilder.d.ts
+++ dist/wpBlockStyleBuilder.d.ts
@@ -5,9 +5,9 @@ declare const wpBlockClassBuilder: (props?: {
     marginBottom?: "none" | "var:preset|spacing|20" | "var:preset|spacing|30" | "var:preset|spacing|40" | "var:preset|spacing|50" | "var:preset|spacing|60" | "var:preset|spacing|70" | "var:preset|spacing|80";
     blockGapX?: "none" | "var:preset|spacing|20" | "var:preset|spacing|30" | "var:preset|spacing|40" | "var:preset|spacing|50" | "var:preset|spacing|60" | "var:preset|spacing|70" | "var:preset|spacing|80";
     blockGapY?: "none" | "var:preset|spacing|20" | "var:preset|spacing|30" | "var:preset|spacing|40" | "var:preset|spacing|50" | "var:preset|spacing|60" | "var:preset|spacing|70" | "var:preset|spacing|80";
-    verticalAlignment?: "center" | "bottom" | "default";
-    orientation?: "default" | "flex" | "horizontal" | "vertical";
-    justifyContent?: "space-between" | "center" | "right" | "default";
+    verticalAlignment?: "default" | "center" | "bottom";
+    orientation?: "flex" | "default" | "horizontal" | "vertical";
+    justifyContent?: "default" | "center" | "right" | "space-between";
     textTransform?: "none" | "capitalize" | "lowercase" | "uppercase";
     fontStyle?: "normal" | "italic";
     textDecoration?: "none" | "line-through" | "underline";
