1f5fd733239f7ad41a1c609fd8cc4cbbac3160f0
Kaelan Smith <kaelancsmith@gmail.com>
2024-06-06 07:59:01 -0700

    update wp rest client type declaration

--- dist/rest/wpRestApiClient.d.ts
+++ dist/rest/wpRestApiClient.d.ts
@@ -1,4 +1,4 @@
-import { type Plugin } from "cloakcms";
+import { type CMSInstance, type Plugin } from "cloakcms";
 declare var wpapi: any;
 export type WPClient = typeof wpapi;
 export type ClientMutationFn = ({ client }: {
@@ -13,5 +13,14 @@ export interface RestApiClientConfig {
     clientMutations?: ClientMutationFn[];
     plugins?: Plugin<RestApiClientConfig>[];
 }
-export declare const wpRestApiClient: (options: RestApiClientConfig) => (incomingConfig: CMSInstance) => Promise<any>;
+export declare const wpRestApiClient: (options: RestApiClientConfig) => (incomingConfig: CMSInstance) => Promise<{
+    client: () => any;
+    url: `http://${string}` | `https://${string}`;
+    adminPath: `/${string}`;
+    contentPath: `/${string}`;
+    name?: string;
+    blockRenderer?: import("cloakcms").BlockRenderer<any, any, Record<string, any>>;
+    meta?: Record<string, any>;
+    plugins?: Plugin<CMSInstance>[];
+}>;
 export {};
